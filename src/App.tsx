326095c55fe9d3371d22860d5779b3e531e08ef0
Hojiakbardevs <abdulhakimovhojiakbar123@gmail.com>
2024-08-23 21:37:07 +0500

    dark light themalari o'rnatildi

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { ModeToggle } from "./components/mode-toggle";
+import { ThemeProvider } from "./components/theme-provider";
+import { Button } from "./components/ui/button";
+
 function App() {
-  return <div className="">
-  Hello 
-  
-  </div>;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ModeToggle></ModeToggle>
+      <Button>Button</Button>
+    </ThemeProvider>
+  );
 }
 
 export default App;
