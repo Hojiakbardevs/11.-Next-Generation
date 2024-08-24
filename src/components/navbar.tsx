8a671afbed77673022719e5c35aa1490c57fbf4f
Hojiakbardevs <abdulhakimovhojiakbar123@gmail.com>
2024-08-24 15:33:03 +0500

    navbar component created

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,20 @@
-import Logo from "@/assets/logo.svg";
+import LogoLight from "@/assets/logo_light.svg";
+import LogoNight from "@/assets/logo_night.svg";
 import { ModeToggle } from "./mode-toggle";
+import { useTheme } from "@/components/theme-provider"; // Import the useTheme hook
+
 export const Navbar = () => {
+  const { theme } = useTheme(); // Destructure the theme from useTheme hook
+
   return (
     <div className="container">
       <div className="flex items-center justify-between">
-        <img src={Logo} alt="Logo icons"/>
+        {/* Conditionally render the logo based on the current theme */}
+        {theme === "dark" ? (
+          <img src={LogoNight} alt="Logo for Dark Mode" />
+        ) : (
+          <img src={LogoLight} alt="Logo for Light Mode" />
+        )}
         <ul className="flex items-center gap-10">
           <li>Home</li>
           <li>About</li>
