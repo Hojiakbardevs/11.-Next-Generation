584482b95ab6df7fd2799591d9fd16755e38386a
Hojiakbardevs <abdulhakimovhojiakbar123@gmail.com>
2024-08-28 23:53:59 +0500

    main component created

--- src/components/Head/button_enter.tsx
+++ src/components/Head/button_enter.tsx
@@ -5,7 +5,7 @@ export function ButtonEnter() {
   return (
     <div>
       <div className="flex w-full max-w-sm items-center">
-        <Input type="text" />
+        <Input type="text" className="w-[250px]"/>
         <Button type="submit">Notify Me ✌️</Button>
       </div>
       <p className="text-sm opacity-80 mt-4">
